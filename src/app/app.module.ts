f62d2a04c07f6fdfc070567f5d6800b667cfc7a3
chandni-kothari <chandni0422@gmail.com>
2021-06-12 20:55:52 +0700

    page 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    PageOneComponent
+    PageOneComponent,
+    PageTwoComponent
   ],
   imports: [
     BrowserModule
